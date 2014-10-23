2a3b73d0bf0f15accd3fbcf6a611783444065b68
Ben Marx <benyamin.marx@gmail.com>
2014-10-23 22:54:10 +0900

    total number of pages for kanji

--- app/controllers/kanjis_controller.rb
+++ app/controllers/kanjis_controller.rb
@@ -2,8 +2,7 @@ class KanjisController < ApplicationController
   before_action :kanji, only: :show
 
   def index
-    
-    render json: Kanji.page(params[:page])
+    render json: { kanji: { kanji_card: Kanji.page(params[:page]), kanji_page_total: Kanji.all.page(1).total_pages } }
   end
 
   def show
