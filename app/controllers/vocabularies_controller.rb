fe921851cc78feea702184fb9e26d08f1ca84623
Ben Marx <benyamin.marx@gmail.com>
2014-10-24 19:01:56 +0900

    paginate vocabulary

--- app/controllers/vocabularies_controller.rb
+++ app/controllers/vocabularies_controller.rb
@@ -1,9 +1,9 @@
 class VocabulariesController < ApplicationController
   def index
     if params[:level].present?
-      render json: { vocabulary: Vocabulary.where(level: params[:level]).page(params[:page]) }
+      render json: { vocabulary: { vocabulary_card: Vocabulary.where(level: params[:level]).page(params[:page]), vocab_page_total: Vocabulary.all.page(1).total_pages } }
     else
-      render json: { vocabulary: Vocabulary.page(params[:page]) }
+      render json: { vocabulary: { vocabulary_card: Vocabulary.page(params[:page]), vocab_page_total: Vocabulary.all.page(1).total_pages } }
     end
   end
 
